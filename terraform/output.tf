b9498e980751296f16c4aca7545167b13d6391bb
alexei-emelin <alexei_emelin@mail.ru>
2023-02-14 16:31:50 +0300

    finish

---
+++ terraform/output.tf
@@ -0,0 +1,8 @@
+#output "access_key" {
+#  value = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+#  sensitive = false
+#}
+#output "secret_key" {
+#  value = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+#  sensitive = false
+#}
